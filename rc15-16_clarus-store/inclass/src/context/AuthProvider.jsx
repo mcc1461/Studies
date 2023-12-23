59a9c35339ad3777c4148361112a09ac8c80582c
mcc1461 <mcoskuncelebi@gmail.com>
2023-12-23 13:45:24 +0300

    login part arranged

--- rc15-16_clarus-store/inclass/src/context/AuthProvider.jsx
+++ rc15-16_clarus-store/inclass/src/context/AuthProvider.jsx
@@ -1,1 +1,27 @@
 //AuthContext
+import React, { useState, createContext } from "react";
+
+const AuthContext = createContext();
+
+
+function AuthProvider({ children }) {
+  const [isAuth, setIsAuth] = useState(false);
+  const [user, setUser] = useState({});
+
+  const login = (email, password) => {
+    setIsAuth(true);
+    setUser({ email, password });
+  };
+
+  const logout = () => {
+    setIsAuth(false);
+    setUser({});
+  };
+
+  const values = { isAuth, user, login, logout };
+
+  return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>;
+}
+
+export { AuthProvider, AuthContext };
+
